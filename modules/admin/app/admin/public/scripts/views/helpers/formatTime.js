5a3b19ae1372bccdf3cffe0ea09e8a49648a0223
sroucheray <stephane.roucheray@free.fr>
2014-10-27 17:56:58 +0100

    re

--- modules/admin/app/admin/public/scripts/views/helpers/formatTime.js
+++
@@ -1,8 +0,0 @@
-define(["hbs/handlebars", "moment"], function(Handlebars, moment) {
-    "use strict";
-    function formatTime(context, options) {
-        return moment.duration(parseInt(context, 10), "milliseconds").format(options.hash.format || "m [minutes] s [secondes]");
-    }
-    Handlebars.registerHelper("formatTime", formatTime);
-    return formatTime;
-});
