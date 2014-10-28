0250a05ae468348eb3591dc57e2a0504b4cdbb94
sroucheray <stephane.roucheray@free.fr>
2014-10-28 10:55:52 +0100

    re

--- modules/admin/app/application/webServer.js
+++ modules/admin/app/application/webServer.js
@@ -98,9 +98,9 @@ WebServer.prototype.setupPublicApp = function() {
         debugReq("%s %s", req.method, req.url);
         next();
     });
-    this.publicApp.use(express.static("bower_components"));
     //this.publicApp.use(express.static("app/admin/public/"));
     //this.publicApp.use("/scripts", express.static("app/public/views/admin/helpers/"));
+    this.publicApp.use("/lib", express.static("bower_components"));
     this.publicApp.use("/views",   express.static("app/public/views"));
     this.publicApp.use("/scripts", express.static("app/public/scripts"));
     this.publicApp.use("/styles",  express.static("app/public/styles"));
