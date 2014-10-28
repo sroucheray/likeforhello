9feda5f522ed4df11ac7505a45a098aba1ad1b50
sroucheray <stephane.roucheray@free.fr>
2014-10-28 09:46:26 +0100

    re

--- modules/admin/app/application/webServer.js
+++ modules/admin/app/application/webServer.js
@@ -101,7 +101,7 @@ WebServer.prototype.setupPublicApp = function() {
     this.publicApp.use(express.static("bower_components"));
     this.publicApp.use(express.static("app/public/"));
     this.publicApp.use(express.static("app/admin/public/"));
-    this.publicApp.use("/scripts", express.static("app/public/views/admin/helpers"));
+    this.publicApp.use("/scripts", express.static("app/public/views/admin/helpers/"));
     this.publicApp.use("/views", express.static("app/public/views"));
     this.publicApp.use("/photos", qt.static(path.join(__dirname, "../..", "tmp/photos")));
     this.publicApp.use(bodyParser.urlencoded({
