72218f6c7d8b6f548f721364be2df1fdc0704ed7
sroucheray <stephane.roucheray@free.fr>
2014-11-13 12:15:20 +0100

    re

--- modules/admin/app/public/scripts/public/accueil_script.js
+++ modules/admin/app/public/scripts/public/accueil_script.js
@@ -132,15 +132,3 @@
         FB.Event.subscribe("edge.remove", unlikeHandler);
     };
 })();
-
-
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/fr_FR/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, "script", "facebook-jssdk"));
