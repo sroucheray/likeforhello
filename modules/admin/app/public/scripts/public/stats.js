1686281f3a569ca8bf62d20848875f8aca40b105
sroucheray <stephane.roucheray@free.fr>
2014-11-20 14:16:27 +0100

    re

--- modules/admin/app/public/scripts/public/stats.js
+++ modules/admin/app/public/scripts/public/stats.js
@@ -1,25 +1,93 @@
+/*global $, _, data_graphic*/
 $(function() {
     "use strict";
-   /* data_graphic({
-        title: "Few Observations",
-        description: "We sometimes have only a few observations. By setting <i>missing_is_zero: true</i>, missing values for a time-series will be interpreted as zeros. In this example, we've overridden the rollover callback to show 'no date' for missing observations and have set the <i>min_x</i> and <i>max_x</i> options in order to expand the date range.",
-        data: data,
-        interpolate: 'basic',
-        missing_is_zero: true,
-        width: torso.width,
-        height: torso.height,
-        right: torso.right,
-        min_x: new Date('2014-01-01'),
-        max_x: new Date('2014-06-01'),
-        target: '#missing-y',
-        x_accessor: 'date',
-        y_accessor: 'value',
-        rollover_callback: function(d, i) {
-            var df = d3.time.format('%b %d, %Y');
-            var date = df(d['date']);
-            var y_val = (d.value == 0) ? 'no data' : d.value;
-            $('#missing-y svg .active_datapoint')
-                .html(date + ' ' + y_val);
-        }
-    });*/
+
+    function update() {
+        $.get("/stats/get").then(function(data) {
+            $("#ofTheDay").attr("src", data.ofTheDay);
+            var imgs = $(".polaroid-list img");
+            imgs.each(function(index, item) {
+                $(item).attr("src", data.lastPhotos[index].filename);
+            });
+
+            var score = {
+                teamA: _.reduce(data.stats, function(memo, datum) {
+                    return datum["Equipe A"] + memo;
+                }, 0),
+                teamB: _.reduce(data.stats, function(memo, datum) {
+                    return datum["Equipe B"] + memo;
+                }, 0),
+                teamC: _.reduce(data.stats, function(memo, datum) {
+                    return datum["Equipe C"] + memo;
+                }, 0)
+            };
+
+            $(".team-a-score").text(score.teamA);
+            $(".team-b-score").text(score.teamB);
+            $(".team-c-score").text(score.teamC);
+
+
+            var stats = [];
+
+            stats.push(_.map(data.stats, function(datum) {
+                var val = {
+                    date: new Date(datum.Jour),
+                    value: datum["Equipe A"]
+                };
+
+                return val;
+            }));
+
+            stats.push(_.map(data.stats, function(datum) {
+                var val = {
+                    date: new Date(datum.Jour),
+                    value: datum["Equipe B"]
+                };
+
+                return val;
+            }));
+
+            stats.push(_.map(data.stats, function(datum) {
+                var val = {
+                    date: new Date(datum.Jour),
+                    value: datum["Equipe C"]
+                };
+
+                return val;
+            }));
+
+            data_graphic({
+                data: stats,
+                width: 919,
+                height: 400,
+                target: "#graphic-data",
+                x_accessor: "date",
+                y_accessor: "value",
+                min_x: new Date("2014-11-15"),
+                max_x: new Date("2014-12-14"),
+                area: false
+            });
+        });
+
+        setTimeout(update, 1000 * 60 * 5);
+    }
+
+    function slide(){
+        var polas = $(".polaroid-list a");
+        var current = polas.eq(0);
+        current.addClass("slide");
+        setTimeout(function(){
+            current.insertAfter(polas.eq(polas.length - 1));
+            setTimeout(function(){
+                current.removeClass("slide");
+                setTimeout(slide, 5000);
+            }, 1000);
+        }, 1000);
+    }
+
+    setTimeout(slide, 2000);
+
+    update();
+
+
 });
