4c80dfd5e2e0ba5223bd5382a4adaabaf593db01
sroucheray <stephane.roucheray@free.fr>
2014-11-24 17:19:53 +0100

    re

--- modules/admin/app/application/webServer.js
+++ modules/admin/app/application/webServer.js
@@ -149,7 +149,7 @@ WebServer.prototype.setupPublicApp = function() {
             res.redirect("https://hello.fb.byperiscope.com/mobile");
         } else {
             res.render("public/redirect", {
-                "app_data" : JSON.stringify(data)
+                id: data.id
             });
         }
     });
