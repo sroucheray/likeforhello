45fd20108114643709dd05463473093d37e975ab
sroucheray <stephane.roucheray@free.fr>
2014-12-15 15:31:22 +0100

    re

--- modules/grab-fans/grab.js
+++ modules/grab-fans/grab.js
@@ -7,44 +7,51 @@ page.onConsoleMessage = function(msg) {
 
 function grab() {
     page.open('https://www.facebook.com/browse/page_fans/?page_id=194889667305745', function() {
-            var total = 0;
-            window.setTimeout(function evaluation() {
-                    page.render("facebook.png");
-                    var list = page.evaluate(function() {
-                            var profiles = [];
-                            window.NodeList.prototype.forEach = window.Array.prototype.forEach;
-                            document.querySelectorAll(".fbProfileBrowserListItem").forEach(function(item, index) {
-                                var profile = item.querySelector("a").getAttribute("href");
-                                profiles.push(profile.match(/https:\/\/www.facebook.com\/(.*)/)[1]);
-                                item.parentNode.removeChild(item);
-                            });
+        var total = 0;
+        page.evaluate(function() {
+            window.document.evaluateProfiles = function() {
+                var profiles = [];
+                window.NodeList.prototype.forEach = window.Array.prototype.forEach;
+                document.querySelectorAll(".fbProfileBrowserListItem").forEach(function(item, index) {
+                    var profile = item.querySelector("a").getAttribute("href");
+                    profiles.push(profile.match(/https:\/\/www.facebook.com\/(.*)/)[1]);
+                    item.parentNode.removeChild(item);
+                });
 
-                            /*document.querySelectorAll(".expandedList .uiList").forEach(function(item, index) {
-                                    if (!item.querySelectorAll("li").length) {
-                                        item.parentNode.removeChild(item);
-                                }
-                            });*/
+                /*document.querySelectorAll(".expandedList .uiList").forEach(function(item, index) {
+                              if (!item.querySelectorAll("li").length) {
+                                  item.parentNode.removeChild(item);
+                          }
+                      });*/
 
-                        window.document.body.scrollTop = document.body.scrollHeight;
+                window.document.body.scrollTop = document.body.scrollHeight;
 
-                        return profiles;
-                    });
+                return profiles;
+            };
+        });
 
-                if (list && list.length) {
-                    for (var i = 0; i < list.length; i++) {
-                        total++;
-                        fs.write("profiles.csv", list[i] + "\n", 'a');
-                    }
-                    console.log("+", list.length, "(total = " + total + ")");
-                } else {
-                    console.log("Should exit");
-                    /*page.evaluate(function() {
+
+        window.setTimeout(function evaluation() {
+            page.render("facebook.png");
+            var list = page.evaluate(function() {
+                return window.document.evaluateProfiles();
+            });
+
+            if (list && list.length) {
+                for (var i = 0; i < list.length; i++) {
+                    total++;
+                    fs.write("profiles.csv", list[i] + "\n", 'a');
+                }
+                console.log("+", list.length, "(total = " + total + ")");
+            } else {
+                console.log("Should exit");
+                /*page.evaluate(function() {
                         window.document.body.scrollTop = 0;
                     });*/
-                }
+            }
 
-                window.setTimeout(evaluation, 400);
-            }, 5000);
+            window.setTimeout(evaluation, 400);
+        }, 5000);
     });
 }
 
